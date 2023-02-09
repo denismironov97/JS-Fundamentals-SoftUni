ce4c828db4938e69594e9b631b4579697386dc32
DenisM <denis.m.031997@gmail.com>
2023-02-09 11:46:02 +0200

    Lab.02V1 uploaded solution

---
+++ Fund Tasks/Basic Syntax Conditional Statements and Loops/Lab/02.ExcellentGradeV1.js
@@ -0,0 +1,13 @@
+function excellentGrade(number){
+    console.log(number >= 5.50 ? 'Excellent' : 'Not excellent');
+}
+
+excellentGrade(5.50);
+excellentGrade(6.00);
+
+excellentGrade(-5.30);
+excellentGrade(-15);
+excellentGrade(2.53333);
+
+excellentGrade(4.00);
+excellentGrade(4.70);
