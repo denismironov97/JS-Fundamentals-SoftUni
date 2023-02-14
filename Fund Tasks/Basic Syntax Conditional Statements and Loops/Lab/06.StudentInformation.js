12cf5229ea34a3a7bafd0225ec554041bc574898
DenisM <denis.m.031997@gmail.com>
2023-02-14 12:52:15 +0200

    Lab.06 uploaded solution

---
+++ Fund Tasks/Basic Syntax Conditional Statements and Loops/Lab/06.StudentInformation.js
@@ -0,0 +1,5 @@
+function printStudentData(studentName, studentAge, studentGrade){
+    console.log(`Name: ${studentName}, Age: ${studentAge}, Grade: ${studentGrade.toFixed(2)}`);
+}
+
+printStudentData('John', 15, 5.54678);
