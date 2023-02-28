3bece5c19e1737c3247d60765c6664994ef0d803
DenisM <denis.m.031997@gmail.com>
2023-02-28 11:42:53 +0200

    Ex.08 uploaded solution

---
+++ Fund Tasks/Basic Syntax Conditional Statements and Loops/Ex/Ex.08.MultiplicationTable.js
@@ -0,0 +1,9 @@
+function printMultiplicationTable(number){
+    for (let i = 1; i < 11; i++) {
+        console.log(`${number} X ${i} = ${number * i}`);
+    }
+}
+
+printMultiplicationTable(5);
+console.log('');
+printMultiplicationTable(2);
