6f0da0c631590f9b545b3ba7c1f838b8d365202b
Denis M <denis.m.031997@gmail.com>
2023-06-19 16:39:29 +0300

    Fixed typo.

--- Fund Tasks/Arrays/Ex/Ex.08.MagicSum.js
+++ Fund Tasks/Arrays/Ex/Ex.08.MagicSum.js
@@ -1,4 +1,4 @@
-function magiSum(array, magicNumber){
+function magicSum(array, magicNumber){
     for (let i = 0; i < array.length; i++) {
         for (let j = i + 1; j < array.length; j++) {
             if(array[i] + array[j] === magicNumber){
@@ -8,6 +8,6 @@ function magiSum(array, magicNumber){
     }
 }
 
-magiSum([1, 7, 6, 2, 19, 23], 8);
-magiSum([14, 20, 60, 13, 7, 19, 8], 27);
-magiSum([1, 2, 3, 4, 5, 6], 6);
+magicSum([1, 7, 6, 2, 19, 23], 8);
+magicSum([14, 20, 60, 13, 7, 19, 8], 27);
+magicSum([1, 2, 3, 4, 5, 6], 6);
